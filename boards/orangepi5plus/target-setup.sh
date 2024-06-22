5e55ba18f8549a6db1ac53d5e6b7be1a5aae2d26
Joseph Lee <joseph@jc-lab.net>
2024-06-22 12:54:30 +0900

    fix

--- boards/orangepi5plus/target-setup.sh
+++ boards/orangepi5plus/target-setup.sh
@@ -14,6 +14,10 @@ FDT_NAME="rk3588-orangepi-5-plus.dtb"
 
 mkdir -p /boot/dtbs/${KERNEL_VER}/
 cp -rf /lib/linux-image-${KERNEL_VER}/rockchip /boot/dtbs/${KERNEL_VER}/rockchip
-ln -s /boot/dtbs/${KERNEL_VER}/rockchip/${FDT_NAME} /boot/dtbs/${KERNEL_VER}/${FDT_NAME}
-ln -s /boot/dtbs/${KERNEL_VER}/rockchip/${FDT_NAME} /boot/dtb
-ln -s /boot/dtbs/${KERNEL_VER}/rockchip/${FDT_NAME} /boot/dtb-${KERNEL_VER}
+( \
+	cd /boot && \
+	ln -s dtbs/${KERNEL_VER}/rockchip/${FDT_NAME} dtbs/${KERNEL_VER}/${FDT_NAME} && \
+	ln -s dtbs/${KERNEL_VER}/rockchip/${FDT_NAME} dtb && \
+	ln -s dtbs/${KERNEL_VER}/rockchip/${FDT_NAME} dtb-${KERNEL_VER} \
+)
+
