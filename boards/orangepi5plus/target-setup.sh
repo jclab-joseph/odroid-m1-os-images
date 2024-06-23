8eb92c88a672d6527425909ce1dfd2492e1270ca
Joseph Lee <joseph@jc-lab.net>
2024-06-23 09:33:08 +0900

    fix: orangepi-5-plus (#1)

--- boards/orangepi5plus/target-setup.sh
+++ boards/orangepi5plus/target-setup.sh
@@ -6,6 +6,13 @@ apt-get install -y /tmp/linux-image.deb
 
 ( \
 	cd /boot && \
+	for name in $(ls vmlinuz-* || true); do \
+		mv $name ${name}.gz && gzip -d ${name}.gz; \
+	done \
+)
+
+( \
+	cd /boot && \
 	ln -s $(find -type f -name "vmlinuz-*" | head -n 1) vmlinuz && \
 	ln -s $(find -type f -name "initrd.img-*" | head -n 1) initrd.img \
 )
@@ -14,6 +21,10 @@ FDT_NAME="rk3588-orangepi-5-plus.dtb"
 
 mkdir -p /boot/dtbs/${KERNEL_VER}/
 cp -rf /lib/linux-image-${KERNEL_VER}/rockchip /boot/dtbs/${KERNEL_VER}/rockchip
-ln -s /boot/dtbs/${KERNEL_VER}/rockchip/${FDT_NAME} /boot/dtbs/${KERNEL_VER}/${FDT_NAME}
-ln -s /boot/dtbs/${KERNEL_VER}/rockchip/${FDT_NAME} /boot/dtb
-ln -s /boot/dtbs/${KERNEL_VER}/rockchip/${FDT_NAME} /boot/dtb-${KERNEL_VER}
+( \
+	cd /boot && \
+	ln -s dtbs/${KERNEL_VER}/rockchip/${FDT_NAME} dtbs/${KERNEL_VER}/${FDT_NAME} && \
+	ln -s dtbs/${KERNEL_VER}/rockchip/${FDT_NAME} dtb && \
+	ln -s dtbs/${KERNEL_VER}/rockchip/${FDT_NAME} dtb-${KERNEL_VER} \
+)
+
